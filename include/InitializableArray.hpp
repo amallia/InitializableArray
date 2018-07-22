335c019f808629ac6c1250bd4713653c11752d47
Antonio Mallia <me@antoniomallia.it>
2018-07-22 15:39:04 -0400

    Added iterator

--- include/InitializableArray.hpp
+++ include/InitializableArray.hpp
@@ -11,39 +11,95 @@ class InitializableArray {
         if (m_s == 0) {
             return false;
         }
-        bool ret = (from[pos] >= 0 and from[pos] <= m_s and to[from[pos]] == pos);
+        bool ret = (m_from[pos] >= 0 and m_from[pos] <= m_s and m_to[m_from[pos]] == pos);
         return ret;
     }
 
    public:
     InitializableArray(size_t size, const T &defaultValue) : m_s(0), m_defaultValue(defaultValue) {
-        values.reserve(size);
-        from.reserve(size);
-        to.reserve(size);
+        m_values.reserve(size);
+        m_from.reserve(size);
+        m_to.reserve(size);
     }
 
-    const T &operator[](size_t pos) const {
-        if (isWritten(pos)) {
-            return values[pos];
+    InitializableArray(const InitializableArray &other)
+        : m_s(other.ms),
+          m_defaultValue(other.m_defaultValue),
+          m_values{other.m_values},
+          m_from(other.m_from),
+          m_to(other.m_to) {}
+
+    InitializableArray &operator=(const InitializableArray &other) {
+        std::swap(m_s, other.m_s);
+        std::swap(m_defaultValue, other.m_defaultValue);
+        std::swap(m_values, other.m_values);
+        std::swap(m_from, other.m_from);
+        std::swap(m_to, other.m_to);
+        return *this;
+    }
+
+    inline const T &operator[](size_t pos) const {
+        if (!isWritten(pos)) {
+            return m_defaultValue;
         }
-        return m_defaultValue;
+        return m_values[pos];
     }
 
-    void insert(size_t pos, const T &value) {
-        values.insert(values.begin() + pos, value);
-        from.insert(from.begin() + pos, m_s);
-        to.insert(to.begin() + m_s, pos);
+    inline void insert(size_t pos, const T &value) {
+        m_values.insert(m_values.begin() + pos, value);
+        m_from.insert(m_from.begin() + pos, m_s);
+        m_to.insert(m_to.begin() + m_s, pos);
         ++m_s;
     }
 
-    size_t size() const { return values.capacity(); }
+    inline size_t size() const { return m_values.capacity(); }
+
+    inline void clear() { m_s = 0; }
+
+    class iterator {
+       public:
+        using iterator_category = typename std::vector<T>::iterator::iterator_category;
+        using value_type = typename std::vector<T>::iterator::value_type;
+        using difference_type = typename std::vector<T>::iterator::difference_type;
+        using pointer = typename std::vector<T>::iterator::pointer;
+        using reference = typename std::vector<T>::iterator::reference;
+
+
+        iterator(const InitializableArray<T> &initializableArray, size_t pos)
+            : m_initializableArray(initializableArray), m_pos(pos) {}
+
+        iterator &operator++() {
+            ++m_pos;
+            return *this;
+        }
+
+        difference_type operator-(iterator &other) {
+            return m_pos - other.m_pos;
+        }
+
+        const T &operator*() { return m_initializableArray[m_pos]; }
+
+        bool operator!=(iterator &other) const {
+            return m_pos != other.m_pos;// and m_initializableArray != other.m_initializableArray;
+        }
+
+       private:
+        const InitializableArray<T> &m_initializableArray;
+        size_t                       m_pos;
+    };
+
+    // iterator begin();
+    // iterator end();
+
+    iterator begin() { return iterator(*this, 0); }
+    iterator end() { return iterator(*this, m_values.capacity()); }
 
    private:
     size_t              m_s;
-    T                   m_defaultValue;
-    std::vector<T>      values;
-    std::vector<size_t> from;
-    std::vector<size_t> to;
+    const T             m_defaultValue;
+    std::vector<T>      m_values;
+    std::vector<size_t> m_from;
+    std::vector<size_t> m_to;
 };
 
 } // namespace arr
--- test/TestInitializableArray.cpp
+++ test/TestInitializableArray.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <chrono>
+#include <iterator>
 
 #include "InitializableArray.hpp"
 
 int main(int argc, char const *argv[])
 {
-    unsigned long long int size = 1000000000;
+    unsigned long long int size = 1000000;
 
     using clock = std::chrono::high_resolution_clock;
     {
@@ -16,14 +17,43 @@ int main(int argc, char const *argv[])
         auto stop = clock::now();
         auto load_time = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start);
         std::cerr << "Array of size " << a.size() << " initialized in " << load_time.count() << " ns" << std::endl;
-        int i = 2;
-        a.insert(0, i);
+        a.insert(0, 2);
         auto tot = 0;
         for (int i = 0; i < a.size(); ++i)
         {
             tot += a[i];
         }
         std::cout << tot << std::endl;
+
+        a.insert(0, 0);
+        tot = 0;
+        for (int i = 0; i < a.size(); ++i)
+        {
+            tot += a[i];
+        }
+        std::cout << tot << std::endl;
+
+        a.clear();
+        tot = 0;
+        for (int i = 0; i < a.size(); ++i)
+        {
+            tot += a[i];
+        }
+        std::cout << tot << std::endl;
+
+        auto it = a.begin();
+        std::cout << *it << std::endl;
+
+        tot = 0;
+        for(auto&& i : a) {
+            tot += i;
+        }
+        std::cout << tot << std::endl;
+
+        std::cout << std::distance(a.begin(), a.end()) << std::endl;
+        std::cout << std::distance(a.end(), a.end()) << std::endl;
+        std::cout << std::distance(a.end(), a.begin()) << std::endl;
+
     }
 
     {
